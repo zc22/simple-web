adf21ea119e70998cb5ad551728cfabeae2d8988
cuiqingq <cuiqingq@grinnell.edu>
2019-09-19 15:47:54 -0500

    static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | Zoe's Portfolio"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | Zoe's Portfolio"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Zoe's Portfolio"
+  end
+  
+end
