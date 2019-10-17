7deead79da6b6791411fbef2b1c8f46931408074
cuiqingq <cuiqingq@grinnell.edu>
2019-10-16 20:10:39 -0500

    hw7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup',  to: 'users#new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Zoe's Portfolio"
+    assert_select "title", "Zoe's Portfolio"
   end
 
   test "should get help" do
