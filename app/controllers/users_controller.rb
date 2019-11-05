30941d71b6d66ba184799d306abff4e3b632ff4d
cuiqingq <cuiqingq@grinnell.edu>
2019-11-04 18:48:15 -0600

    testing error message views in production mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successful save
+    else
+      render 'new'
+    end
+  end
+  
+    private
+      def user_params
+        params.require(:user).permit(:name, :email)
+      end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'static_pages/resume'
   
-  get 'static_pages/about'
+  get 'static_pages/about_me'
   
   resources :users
 end
